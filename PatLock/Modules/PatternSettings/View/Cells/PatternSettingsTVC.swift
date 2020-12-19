74d62eb3c45def7e145a2b3e5f99c900a1ce68f3
shndrs <sahandraeisi1994@gmail.com>
2020-12-19 12:41:07 +0330

    Added Register

---
+++ PatLock/Modules/PatternSettings/View/Cells/PatternSettingsTVC.swift
@@ -0,0 +1,20 @@
+//
+//  PatternSettingsTVC.swift
+//  PatLock
+//
+//  Created by Sahand Raeisi on 12/19/20.
+//
+
+import UIKit
+
+final class PatternSettingsTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
